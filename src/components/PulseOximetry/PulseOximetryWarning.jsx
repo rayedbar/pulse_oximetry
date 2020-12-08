aa3b237a478ce5e37976523b12a2c30789aa70d4
M Tanjid Hasan Tonmoy <tanjid.hasan@therapservices.net>
2020-12-08 15:22:21 +0600

    refactor: Link to alert recipient page on pulse ox warning

--- src/components/PulseOximetry/PulseOximetryWarning.jsx
+++ src/components/PulseOximetry/PulseOximetryWarning.jsx
@@ -1,22 +1,14 @@
 import React from "react";
-import { useQuery, gql } from "@apollo/client";
+import { Link } from "react-router-dom";
 import { Grid } from "@material-ui/core";
 import { Alert, AlertTitle } from "@material-ui/lab";
 
-import ProgressBar from "../Shared/ProgressBar";
 import { getPulseOximetryRange } from "../../utils/functions";
-import { listAlertRecipients as LIST_ALERT_RECIPIENTS } from "../../graphql/queries";
+import { URL } from "../../utils/constants";
 
 const PulseOximetryWarning = ({ individual }) => {
   const latestPulseOximetry = individual?.pulseOximetry.items[0];
   const pulseOximetryRange = getPulseOximetryRange(individual);
-
-  const { loading, error, data } = useQuery(gql(LIST_ALERT_RECIPIENTS), {
-    variables: { limit: 1 },
-  });
-
-  if (loading) return <ProgressBar />;
-  if (error) return `Error! ${error.message}`;
 
   return latestPulseOximetry ? (
     <Grid item container direction="column" spacing={2}>
@@ -25,9 +17,7 @@ const PulseOximetryWarning = ({ individual }) => {
           <Alert severity="error">
             <AlertTitle>SpO2 Warning</AlertTitle>
             The latest SpO2 suggests a medical issue may be present. Please
-            report to{" "}
-            {`${data.listAlertRecipients.items[0].firstName} ${data.listAlertRecipients.items[0].firstName}`}{" "}
-            at <strong>{data.listAlertRecipients.items[0].phone}</strong>
+            report to an <Link to={URL.ALERT_RECIPIENTS}>Alert Recipient</Link>
           </Alert>
         </Grid>
       ) : null}
@@ -38,9 +28,7 @@ const PulseOximetryWarning = ({ individual }) => {
           <Alert severity="error">
             <AlertTitle>Heart Rate Warning</AlertTitle>
             The latest Heart Rate suggests a medical issue may be present.Please
-            report to{" "}
-            {`${data.listAlertRecipients.items[0].firstName} ${data.listAlertRecipients.items[0].firstName}`}{" "}
-            at <strong>{data.listAlertRecipients.items[0].phone}</strong>
+            report to an <Link to={URL.ALERT_RECIPIENTS}>Alert Recipient</Link>
           </Alert>
         </Grid>
       ) : null}
